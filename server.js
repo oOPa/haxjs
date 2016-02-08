d92d47d85ae072753c0d03ac8754f947558ea14d
Ubuntu <azureuser@haxjs.haxjs.f10.internal.cloudapp.net>
2016-02-08 15:35:12 +0000

    added package.json, changed routes, changed entry point to server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require("express")
+app = express();
+//app.use(express.static('.'));
+app.get("/loader.js", function(req,res){res.sendFile(__dirname+"/loader.js");});
+/** should use cdn**/
+app.get("/pixi.min.js", function(req,res){res.sendFile(__dirname+"/pixi.min.js");});
+/** pixi vectors **/
+app.get("/pixi.vector.js", function(req,res){res.sendFile(__dirname+"/pixi.vector.js");});
+app.get("/", function(req,res){res.sendFile(__dirname+"/index.html");});
+app.listen(8888);
