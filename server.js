e1311782e5a6f30ee5d844853da2c31a3f0b2ab2
Ubuntu <azureuser@haxjs.haxjs.f10.internal.cloudapp.net>
2016-02-09 03:37:25 +0000

    change entry point to entry.html and allowed different port to be used

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ var express = require("express")
 app = express();
 //app.use(express.static('.'));
 app.get("/loader.js", function(req,res){res.sendFile(__dirname+"/loader.js");});
+app.get("/navigation.js", function(req,res){res.sendFile(__dirname+"/navigation.js");});
 /** should use cdn**/
 app.get("/pixi.min.js", function(req,res){res.sendFile(__dirname+"/pixi.min.js");});
 /** pixi vectors **/
 app.get("/pixi.vector.js", function(req,res){res.sendFile(__dirname+"/pixi.vector.js");});
-app.get("/", function(req,res){res.sendFile(__dirname+"/index.html");});
+app.get("/", function(req,res){res.sendFile(__dirname+"/entry.html");});
 app.get("/create_room",function (req,res){
 	
 })
-app.listen(8888);
+app.listen(process.env.port || 8888);
