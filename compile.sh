73bc99efb28b9d60fba6fc6cbd0c67eb1643243c
Oopa cygwin <oopa@users.noreply.github.com>
2016-03-29 22:36:20 +0100

    compile script

---
+++ compile.sh
@@ -0,0 +1,5 @@
+cp src/js/constants.js bin/constants.js
+cp src/html/entry.html bin/index.html
+cat src/js/loader.js src/js/controller.js src/js/controller-net.js src/js/network.js src/js/physics.js src/js/player.js src/js/player.net.js src/js/rendering.js src/js/rendering.net.js src/js/ui.js > tmp.full.js
+java -jar yuicompressor.jar tmp.full.js -o bin/loader.min.js
+rm tmp.full.js
